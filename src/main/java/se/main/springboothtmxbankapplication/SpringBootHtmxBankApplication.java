3c417b9e5314e1f633f2c2e4767662e41156a9a8
martiningeson <oi12min@cs.umu.se>
2024-05-20 14:26:01 +0200

    Spring initializer

---
+++ src/main/java/se/main/springboothtmxbankapplication/SpringBootHtmxBankApplication.java
@@ -0,0 +1,13 @@
+package se.main.springboothtmxbankapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHtmxBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHtmxBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/main/springboothtmxbankapplication/SpringBootHtmxBankApplicationTests.java
@@ -0,0 +1,13 @@
+package se.main.springboothtmxbankapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHtmxBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
